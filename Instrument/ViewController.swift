e1f126e0c0261f8748d8c36afb32e0926c68e9be
Hamzah Mugharbil <Hamzah@hamzahs-mbp.lan>
2017-08-10 11:54:10 +0300

    Initial Commit

---
+++ Instrument/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instrument
+//
+//  Created by Hamzah Mugharbil on 8/10/17.
+//  Copyright © 2017 Hamzah Mugharbil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
